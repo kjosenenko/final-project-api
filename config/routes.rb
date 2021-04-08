19a3bc03470fccbf344bfb9553fabaabb9c9c2d4
KJ Osenenko <karljosenenko@gmail.com>
2021-04-08 15:09:13 -0600

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :replies
-  resources :posts
+  resources :posts do
+    resources :replies
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
